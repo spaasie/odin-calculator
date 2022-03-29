4b18ceb4a2dcfcadcda11af7a577d2880e21a721
spaasie <68701772+spaasie@users.noreply.github.com>
2022-03-29 15:21:19 +0200

    add function operate

--- app.js
+++ app.js
@@ -17,3 +17,18 @@ const multiply = function(a,b) {
 const divide = function(a, b) {
   return a / b;
 }
+
+const operate = function(operator, a, b) {
+  switch (operator) {
+    case "add":
+      return add(a, b);
+    case "subtract":
+      return subtract(a, b);
+    case "multiply":
+      return multiply(a, b);
+    case "divide":
+      return divide(a, b);
+    default:
+      console.log(`${operator} is not a valid operator`)
+  }
+}
