3b114c5cb6e5a6900a650be9db6f210539d029ec
spaasie <68701772+spaasie@users.noreply.github.com>
2022-03-29 15:05:39 +0200

    add functions 'add', 'subtract', 'multiply', 'divide' to app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const test = document.querySelector("#test");
 
 test.textContent = "hello and welcome from js"
+
+const add = function(a, b) {
+  return a + b;
+}
+
+const subtract = function(a, b) {
+  return a - b;
+}
+
+const multiply = function(a,b) {
+  return a * b;
+}
+
+const divide = function(a, b) {
+  return a / b;
+}
